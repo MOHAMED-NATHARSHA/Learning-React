ad876275ddc9c8e2df600c1e6e31c2560b50348b
Mohamed Natharsha <mohamednatharsha27@gmail.com>
2025-09-13 22:53:02 +0800

    Day-2

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
     const rootEl = document.getElementById('root');
     const rootElement = ReactDOM.createRoot(rootEl);
-    const headingReactEl = React.createElement("p", {id:"heading"}, "Hello World from React");
+    const headingReactEl = React.createElement("strong", {id:"heading"}, "Hello World from React");
     rootElement.render(headingReactEl);
+
+
+    
